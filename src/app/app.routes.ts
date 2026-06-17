d4805dddf3601529ebe1b101f1e3cd68844cf292
Mansoor Ahmad <mansoor.ahmadd13@gmail.com>
2026-06-17 17:53:00 +0500

    add theme and login page with reactive form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login,
+        title: 'Login · Pakistan Sweet Home CRM'
+    },
+    
+];
---
+++ src/app/login/login.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required]
+    })
+  })
+
+  onSubmit() {
+    
+  }
+
+}
